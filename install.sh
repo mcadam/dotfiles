5df1b5eef24d4dfa62bcf71669845a31fcdc88f6
Adam Guldemann <adam.guldemann@gmail.com>
2020-04-21 11:07:52 +0200

    Update rm leftover files

--- install.sh
+++ install.sh
@@ -52,6 +52,7 @@ sudo apt install -y neovim
 # install ripgrep
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 sudo dpkg -i ripgrep_11.0.2_amd64.deb
+sudo rm ripgrep_11.0.2_amd64.deb
 
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
