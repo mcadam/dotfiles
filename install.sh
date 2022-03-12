57936bfc4c44031c937a654fe5d8622e31ca8b88
Adam Guldemann <adam.guldemann@gmail.com>
2022-03-12 16:15:34 +0000

    Fix ubuntu

--- install.sh
+++ install.sh
@@ -39,14 +39,12 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 # install microk8s
-snap install microk8s --classic --stable
+sudo snap install microk8s --classic --stable
 microk8s enable dns ingress storage metrics-server
-snap alias microk8s.kubectl kubectl
+sudo snap alias microk8s.kubectl kubectl
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 mkdir -p ~/.kube
 microk8s config > ~/.kube/config
@@ -59,11 +57,11 @@ tar xvf k9s_Linux_x86_64.tar.gz && mv k9s /usr/local/bin/ && rm k9s_Linux_x86_64
 
 wget https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_x86_64.tar.gz
 wget https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_linux_x86_64.tar.gz
-tar xvf kubectx_v0.9.4_linux_x86_64.tar.gz && mv kubectx /usr/local/bin/kctx && rm kubectx_v0.9.4_linux_x86_64.tar.gz
-tar xvf kubens_v0.9.4_linux_x86_64.tar.gz && mv kubens /usr/local/bin/kns && rm kubens_v0.9.4_linux_x86_64.tar.gz
+tar xvf kubectx_v0.9.4_linux_x86_64.tar.gz && sudo mv kubectx /usr/local/bin/kctx && rm kubectx_v0.9.4_linux_x86_64.tar.gz
+tar xvf kubens_v0.9.4_linux_x86_64.tar.gz && sudo mv kubens /usr/local/bin/kns && rm kubens_v0.9.4_linux_x86_64.tar.gz
 
-echo > /var/snap/microk8s/current/args/kubectl-env
-iptables -P FORWARD ACCEPT
+sudo echo > /var/snap/microk8s/current/args/kubectl-env
+sudo iptables -P FORWARD ACCEPT
 
 # update user to groups
 sudo usermod -aG sudo $USER
